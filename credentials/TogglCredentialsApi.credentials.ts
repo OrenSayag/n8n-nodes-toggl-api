3cb35da88438091b81de1e096677bcd8491b6b08
Oren Sayag <orensayag500@gmail.com>
2025-06-30 22:07:59 +0300

    modify

--- credentials/TogglCredentialsApi.credentials.ts
+++ credentials/TogglCredentialsApi.credentials.ts
@@ -1,4 +1,9 @@
-import { ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+    IAuthenticateGeneric,
+    ICredentialTestRequest,
+    ICredentialType,
+    INodeProperties,
+} from 'n8n-workflow';
 
 export class TogglCredentialsApi implements ICredentialType {
     name = 'togglCredentialsApi';
@@ -19,24 +24,15 @@ export class TogglCredentialsApi implements ICredentialType {
         },
     ];
 
-    // This credential is currently not used by any node directly
-    // but the HTTP Request node can use it to make requests.
-    // The credential is also testable due to the `test` property below
-    // authenticate: IAuthenticateGeneric = {
-    //     type: 'generic',
-    //     properties: {
-    //         // auth: {
-    //         // 	username: '={{ $credentials.username }}',
-    //         // 	password: '={{ $credentials.password }}',
-    //         // },
-    //         // qs: {
-    //         // 	// Send this as part of the query string
-    //         // 	n8n: 'rocks',
-    //         // },
-    //     },
-    // };
+    authenticate: IAuthenticateGeneric = {
+        type: 'generic',
+        properties: {
+            headers: {
+                apikey: '={{$credentials.api_key}}',
+            },
+        },
+    };
 
-    // The block below tells how this credential can be tested
     test: ICredentialTestRequest = {
         request: {
             baseURL: 'https://api.track.toggl.com/api/v9/me',
